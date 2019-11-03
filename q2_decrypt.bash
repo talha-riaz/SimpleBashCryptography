0a715b2047203178ccb5d549407bedc8a92bf4f7
Talha Riaz <talhariaz3077@Talhas-MacBook-Pro.local>
2019-11-02 21:23:06 -0400

    commit

--- q2_decrypt.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-  
-large=$(cat $1 | grep A)
-small=$(cat $1 | grep a)
-message=$(echo $(cat $2))
-
-echo $(echo $message | tr "${large}${small}" "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
--- q2_encrypt.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/Users/talhariaz3077/Documents/GitHub/A1_solutions/q2_encrypt.bash
-large=$(cat $1 | grep A)
-small=$(cat $1 | grep a)
-message=$(echo $(cat $2))
-
-echo $(echo $message | tr "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" "${large}${small}")
